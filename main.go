acbed29b6c5538b7c0c5d2d8eb958bb667838838
ArtemHvozdov <naltuhov4@gmail.com>
2025-09-03 17:31:04 +0300

    wrote functions for summing up the winner's results in the DB and for sending the results to the chat. Tested, everything works as it should.

--- main.go
+++ main.go
@@ -112,6 +112,7 @@ func main() {
 	bot.Handle("/create", handlers.CreateCollageFromResultsImageNine(bot))
 	// Register handler for showing results
 	bot.Handle("/subtask_results", handlers.SendSubtaskResultsToChat(bot))
+	bot.Handle("/subtask10_results", handlers.ShowSubtask10Results(bot))
 
 	// bot.Handle(&telebot.InlineButton{Data: "color_answer_1"}, handlers.HandleColorAnswer(bot))
     // bot.Handle(&telebot.InlineButton{Data: "color_answer_2"}, handlers.HandleColorAnswer(bot))
